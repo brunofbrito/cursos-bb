072afba5322816bc3e631531f3077dead945ad7f
Bruno Brito <brunofbrito@gmail.com>
2020-03-16 13:51:42 +0000

    Add global styles

--- src/@hangindev/gatsby-theme-courses/styled/globalStyle.js
+++ src/@hangindev/gatsby-theme-courses/styled/globalStyle.js
@@ -1,12 +1,57 @@
 const globalStyle = `
-  *, *::before, *::after {
-    box-sizing: border-box;
-  }
-  body {
-    background: #fbfbfb;
-    color: rgba(0,0,0,.87);
-    font-family: 'Open Sans', sans-serif;
-  }
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+body {
+  font-family: 'Open Sans', sans-serif;
+}
+
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+ol,
+ul {
+  margin: 0;
+  padding: 0;
+  font-weight: normal;
+  color: rgb(52, 63, 72);
+}
+
+ol,
+ul {
+  list-style: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+a {
+  text-decoration: none;
+}
+
+.active div {
+  background: #ff9100;
+}
+
+.active p {
+  color: white;
+}
+
 
 `
 
