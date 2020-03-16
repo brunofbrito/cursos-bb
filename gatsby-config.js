d1eed3b95c2681b51a3d0bb6101fe047e2ed11e0
Bruno Brito <brunofbrito@gmail.com>
2020-03-16 13:44:14 +0000

    Add plugin to config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@the_bammer`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
