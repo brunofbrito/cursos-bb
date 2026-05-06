132eed374cc569b994a55e1418ffd06d13b3c7e1
Bruno Brito <brunofbrito@gmail.com>
2026-05-06 01:01:13 +0100

    Make lesson nav arrows clickable

--- src/@hangindev/gatsby-theme-courses/components/VideoNav.js
+++ src/@hangindev/gatsby-theme-courses/components/VideoNav.js
@@ -15,11 +15,16 @@ const Nav = styled.nav`
   padding: 24px 0;
   display: flex;
   align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+  }
   a,
   svg {
     color: var(--grey);
   }
-  a:hover {
+  a:hover,
+  a:hover svg {
     color: var(--accent);
   }
   svg {
@@ -48,21 +53,21 @@ function VideoNav() {
     <NavBar>
       {previousLesson ? (
         <Nav>
-          <svg
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="far"
-            data-icon="long-arrow-alt-left"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-          >
-            <path
-              fill="currentColor"
-              d="M107.515 150.971L8.485 250c-4.686 4.686-4.686 12.284 0 16.971L107.515 366c7.56 7.56 20.485 2.206 20.485-8.485v-71.03h308c6.627 0 12-5.373 12-12v-32c0-6.627-5.373-12-12-12H128v-71.03c0-10.69-12.926-16.044-20.485-8.484z"
-            ></path>
-          </svg>
           <Link to={previousLesson.slug}>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="far"
+              data-icon="long-arrow-alt-left"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path
+                fill="currentColor"
+                d="M107.515 150.971L8.485 250c-4.686 4.686-4.686 12.284 0 16.971L107.515 366c7.56 7.56 20.485 2.206 20.485-8.485v-71.03h308c6.627 0 12-5.373 12-12v-32c0-6.627-5.373-12-12-12H128v-71.03c0-10.69-12.926-16.044-20.485-8.484z"
+              ></path>
+            </svg>
             <span>anterior</span>
           </Link>
         </Nav>
@@ -73,21 +78,21 @@ function VideoNav() {
         <Nav>
           <Link to={nextLesson.slug}>
             <span>seguinte</span>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="far"
+              data-icon="long-arrow-alt-right"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path
+                fill="currentColor"
+                d="M340.485 366l99.03-99.029c4.686-4.686 4.686-12.284 0-16.971l-99.03-99.029c-7.56-7.56-20.485-2.206-20.485 8.485v71.03H12c-6.627 0-12 5.373-12 12v32c0 6.627 5.373 12 12 12h308v71.03c0 10.689 12.926 16.043 20.485 8.484z"
+              ></path>
+            </svg>
           </Link>
-          <svg
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="far"
-            data-icon="long-arrow-alt-right"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-          >
-            <path
-              fill="currentColor"
-              d="M340.485 366l99.03-99.029c4.686-4.686 4.686-12.284 0-16.971l-99.03-99.029c-7.56-7.56-20.485-2.206-20.485 8.485v71.03H12c-6.627 0-12 5.373-12 12v32c0 6.627 5.373 12 12 12h308v71.03c0 10.689 12.926 16.043 20.485 8.484z"
-            ></path>
-          </svg>
         </Nav>
       )}
     </NavBar>
